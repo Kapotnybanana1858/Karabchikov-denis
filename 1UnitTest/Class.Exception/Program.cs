a5cbe2cc0c60c81ee8cea77b5a16d6fe1c355c97
Kapotnybanana1858 <90246832+Kapotnybanana1858@users.noreply.github.com>
2021-09-09 12:58:28 +0300

    Add files via upload

---
+++ 1UnitTest/Class.Exception/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+
+            static void Main(string[] args)
+            {
+                double a, b;
+                try
+                {
+                    Console.WriteLine("Число 1: ");
+                    a = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Число 2: ");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                    if (b == 0)
+                        Console.WriteLine("Вы не можете делить на ноль ");
+                    else
+                        Console.WriteLine("Результат :" + " " + a / b);
+
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильный ввод");
+                }
+                Console.WriteLine("Размер типа Integer:" + " " + sizeof(int));
+                Console.WriteLine("Размер типо Float:" + " " + sizeof(float));
+            }
+        }
+    }
+
+
