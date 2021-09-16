ad9f5556ddefe244a7ade5bf35662387b33fa11a
Kapotnybanana1858 <90246832+Kapotnybanana1858@users.noreply.github.com>
2021-09-16 11:18:09 +0300

    Add files via upload

---
+++ 1UnitTest/ClassCalculator/CalculatorCheckerTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorCheckerTests
+    {
+        [TestMethod()]
+        public void ValidateCalculatorTest_plus_ReturnTrue()
+        {
+            double a = 2;
+            double b = 4;
+            string operation = "+";
+            double result = 6;
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation );
+            Assert.AreEqual(result, actual);
+
+        }
+
+        
+        [TestMethod]
+        public void ValidateCalculatorTest_Minus_ReturnTrue()
+        {
+            double a = 10;
+            double b = 5;
+            string operation = "-";
+            double result = 5;
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation );
+            Assert.AreEqual(result, actual);
+        }
+        
+        [TestMethod]
+        public void ValidateCalculatorTest_multiply_ReturnTrue()
+        {
+            double a = 5;
+            double b = 5;
+            string operation = "*";
+            double result = 25;
+            
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
+            Assert.AreEqual(result, actual);
+        }
+        
+        [TestMethod]
+        public void ValidateCalculatorTest_divide_ReturnTrue()
+        {
+            double a = 2.5;
+            double b = 5;
+            string operation = "/";
+            double result = 0.5;
+            
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation );
+            Assert.AreEqual(result, actual);
+        }
+
+        [TestMethod()]
+        [ExpectedException (typeof(DivideByZeroException),"Вы не можете делить на ноль ")]
+        public void ValidateCalculatorTest_divide_ReturnFalse()
+        {
+            double a = 25;
+            double b = 0;
+            
+            
+            string operation = "/";
+            double actual = CalculatorChecker.ValidateCalculator(a, b, operation );
+            
+        }
+
+
+
+
+    }
+}
